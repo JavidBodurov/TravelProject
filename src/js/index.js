6e5b6373074c607a0f4a35fc14bc8df7209c61d6
Javid <javid.bodurov93@gmail.com>
2025-01-19 19:41:11 -0700

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,42 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  parallax: true,
+  speed: 1000,
+
+  keyboard: {
+    enabled: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.slider-controls__count',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#sliderPrev',
+    prevEl: '#sliderNext',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+
